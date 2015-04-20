1de7fe702409940a20d5c092a189f676d4682433
Sprotte <sprottemail@googlemail.com>
2015-04-20 15:01:02 +0200

    Init

---
+++ Calculator/Aufgabe2_PaulSprotteTests/Aufgabe2_PaulSprotteTests.swift
@@ -0,0 +1,36 @@
+//
+//  Aufgabe2_PaulSprotteTests.swift
+//  Aufgabe2_PaulSprotteTests
+//
+//  Created by Paul Sprotte on 10.04.15.
+//  Copyright (c) 2015 Paul Sprotte. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Aufgabe2_PaulSprotteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
