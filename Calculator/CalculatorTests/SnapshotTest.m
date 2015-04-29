03dc6dc208aed9a666005c6c7c928def486a0df8
Sprotte <sprottemail@googlemail.com>
2015-04-29 15:55:48 +0200

    FBSnapshot working

--- Calculator/CalculatorTests/SnapshotTest.m
+++ Calculator/CalculatorTests/SnapshotTest.m
@@ -7,8 +7,8 @@
 //
 
 #import "FBSnapshotTestCase.h"
-
-
+#import "FBExampleView.h"
+#import "ViewController.h"
 @interface SnapshotTest : FBSnapshotTestCase
 
 @end
@@ -17,10 +17,11 @@
 
 - (void)setUp {
     [super setUp];
+    
     // Flip this to YES to record images in the reference image directory.
     // You need to do this the first time you create a test and whenever you change the snapshotted views.
     // Tests running in record mode will allways fail so that you know that you have to do something here before you commit.
-    self.recordMode = YES;
+    self.recordMode = NO;
 
 }
 
@@ -30,7 +31,12 @@
 }
 
 - (void)testExample {
+    ViewController *vc = [ViewController new];
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    vc = [storyboard instantiateViewControllerWithIdentifier:@"ViewController"];
+    [vc performSelectorOnMainThread:@selector(loadView) withObject:nil waitUntilDone:YES];
     
+    FBSnapshotVerifyView(vc.view, nil);
 }
 
 
